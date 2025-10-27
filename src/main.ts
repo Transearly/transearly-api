28c9b4f04a9e9ed79c7f93e0a1b51eaf06f64555
minhthang165 <minhthangqhqn@gmail.com>
2025-10-27 22:33:54 +0700

    refactor api

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,35 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { configSwagger } from './configs/api-docs.config';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
-import { Request, Response } from 'express';
 
-let cachedApp: NestExpressApplication;
-
-async function createApp() {
-  const logger = new Logger('NestApplication');
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    bodyParser: true,
-  });
-
+async function bootstrap() {
+  const logger = new Logger(bootstrap.name);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const reflector = app.get(Reflector);
   app.useGlobalInterceptors(new ResponseInterceptor(reflector));
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
-
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: '*',
   });
-
   configSwagger(app);
+ 
   app.useStaticAssets(join(__dirname, './served'));
-
-  await app.init();
-  logger.log(`✅ NestJS initialized`);
-  return app;
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  
+  const port = process.env.PORT || 4000;
+  await app.listen(port, () =>
+    logger.log(`🚀 Server running on: http://localhost:${port}/api-docs`),
+  );
 }
-
-/**
- * ✅ Handler cho Vercel (Serverless)
- */
-export default async function handler(req: Request, res: Response) {
-  if (!cachedApp) {
-    cachedApp = await createApp();
-  }
-  const expressApp = cachedApp.getHttpAdapter().getInstance();
-  return expressApp(req, res);
-}
-
-/**
- * ✅ Nếu chạy local => bật app.listen()
- */
-if (!process.env.VERCEL) {
-  (async () => {
-    const app = await createApp();
-    const port = process.env.PORT || 4000;
-    await app.listen(port);
-    console.log(`🚀 Server running at http://localhost:${port}/api-docs`);
-  })();
-}
+bootstrap();
